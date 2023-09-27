248a16e25b898f032d00f7f8f5dd50b4110b2133
ChoiWheatley <chltmdgus604@naver.com>
2023-09-27 17:28:10 +0900

    hotfix(list_init)

--- threads/thread.c
+++ threads/thread.c
@@ -421,6 +421,7 @@ init_thread (struct thread *t, const char *name, int priority) {
   strlcpy (t->name, name, sizeof t->name);
   t->tf.rsp = (uint64_t) t + PGSIZE - sizeof (void *);
   t->priority = priority;
+  list_init(&t->donation_list);
   t->magic = THREAD_MAGIC;
 }
 
