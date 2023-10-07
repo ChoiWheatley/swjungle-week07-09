fb3f7d19e6762b65d63e4494bae39839da5e890c
ChoiWheatley <chltmdgus604@naver.com>
2023-10-07 16:00:37 +0900

    feat(user-memory-access) Add get/put_user

--- include/userprog/exception.h
+++ include/userprog/exception.h
@@ -1,5 +1,7 @@
 #ifndef USERPROG_EXCEPTION_H
 #define USERPROG_EXCEPTION_H
+#include <stdbool.h>
+#include "include/threads/interrupt.h"
 
 /* Page fault error code bits that describe the cause of the exception.  */
 #define PF_P 0x1    /* 0: not-present page. 1: access rights violation. */
@@ -9,4 +11,7 @@
 void exception_init (void);
 void exception_print_stats (void);
 
+/**SECTION - Additional Decl*/
+void check_address(const void *uaddr);
+/**!SECTION - Additional Decl*/
 #endif /* userprog/exception.h */
