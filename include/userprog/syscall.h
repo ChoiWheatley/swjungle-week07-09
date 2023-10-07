b8c3833c9098bb55d4512326e10fc80a7af90617
ChoiWheatley <chltmdgus604@naver.com>
2023-10-07 17:04:36 +0900

    Add declaration of syscall.h

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -1,6 +1,25 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include <stdbool.h>
+
 void syscall_init (void);
+
+void exit(int status);
+bool create(const char *file, unsigned initial_size);
+bool remove(const char *file);
+int open(const char *file);
+int filesize(int fd);
+int read(int fd, void *buffer, unsigned size);
+int write(int fd, const void *buffer, unsigned size);
+void seek(int fd, unsigned position);
+unsigned tell(int fd);
+void close(int fd);
+void halt(void);
+void exit(int status);
+pid_t fork(const char *thread_name);
+int exec(const char *file);
+int wait(pid_t pid);
+
 
 #endif /* userprog/syscall.h */
