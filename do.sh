9dae82b26732c94e58fb62bd37ba9c3f02125338
ChoiWheatley <chltmdgus604@naver.com>
2023-10-20 17:17:53 +0900

    fixme(page-merge-par) commented out all locks

--- do.sh
+++ do.sh
@@ -1,46 +1,6 @@
 # #!/bin/bash
-# tests=(
-#   priority-change           #pass
-#   priority-donate-one       #pass
-#   priority-donate-multiple  #pass
-#   priority-donate-multiple2 #pass
-#   priority-donate-nest      #pass
-#   priority-donate-lower     #pass
-#   priority-donate-chain     #pass
-#   priority-fifo             #pass
-#   priority-preempt          #pass
-#   priority-sema             #pass
-#   priority-condvar          #pass
-#   priority-donate-sema      #pass
-# )
-# workspace_root=$(pwd)
-# log_dir="$workspace_root/log"
-
-# if [ ! -d "$log_dir" ]; then
-#   mkdir -p "$log_dir"
-# fi
-
-# cd threads
-# make clean >/dev/null 2>&1
-# make >/dev/null 2>&1
-# cd build
-
-# for test in ${tests[@]}; do
-#   pintos -- -q run $test > "$log_dir/$test.log"
-#   echo written output in "$log_dir/$test.log"
-# done
 cd vm
 make clean
 make -j $(nproc --all)
 cd build
-# pintos-mkdisk filesys.dsk 10
-# pintos --gdb --fs-disk=10 -p tests/userprog/args-single:args-single -- -q -f run 'args-single onearg'
-# pintos --fs-disk filesys.dsk -p tests/userprog/args-single:args-single -- -f run 'args-single onearg'
-# pintos --fs-disk filesys.dsk -p tests/userprog/fork-once:fork-once --gdb -- -f run 'fork-once'
-# pintos --fs-disk=10 -p tests/userprog/read-normal:read-normal -p ../../tests/userprog/sample.txt:sample.txt -- -q -f run read-normal
-# pintos --fs-disk=10 -p tests/userprog/close-normal:close-normal -p ../../tests/userprog/sample.txt:sample.txt --gdb -- -q -f  run 'close-normal'
-# pintos --gdb -v -k --fs-disk=10 -p tests/userprog/exec-once:exec-once -p tests/userprog/child-simple:child-simple -- -q   -f run exec-once
-# pintos --gdb -v -k --fs-disk=10 -p tests/userprog/exec-missing:exec-missing -- -q   -f run exec-missing
-# pintos --gdb -v -k --fs-disk=10 -p tests/userprog/read-zero:read-zero -p ../../tests/userprog/sample-text:sample-text -- -q -f run read-zero
-# pintos $1 -v -k --fs-disk=10 -p tests/userprog/args-none:args-none --swap-disk=4 -- -q   -f run args-none
-pintos -v -k -T 60 -m 20   --fs-disk=10 -p tests/userprog/args-many:args-many --swap-disk=4 -- -q   -f run 'args-many a b c d e f g h i j k l m n o p q r s t u v'
+pintos $1 -v -k -m 20   --fs-disk=10 -p tests/vm/page-merge-par:page-merge-par -p tests/vm/child-sort:child-sort --swap-disk=10 -- -q   -f run page-merge-par
